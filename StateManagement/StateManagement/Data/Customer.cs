0191088257943d9472fc73ba3ccf1d86a8c156b8
hintsofttech <71686624+hintsofttech@users.noreply.github.com>
2022-06-13 20:55:23 +0545

    Add files via upload

---
+++ StateManagement/StateManagement/Data/Customer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+public record Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; } 
+    public string LastFocusedId { get; set; }   
+}
